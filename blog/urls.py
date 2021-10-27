dca76e4157839b202360895badc11f785553697b
lea.arturi <lea.arturi@gmail.com>
2021-10-27 12:27:48 -0300

    Login & Logout

--- blog/urls.py
+++ blog/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     re_path('', include('applications.users.urls')),
     re_path('', include('applications.home.urls')),
     re_path('', include('applications.entrada.urls')),
+    re_path('', include('applications.favoritos.urls')),
 
     # URLs para ckeditor
     re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
